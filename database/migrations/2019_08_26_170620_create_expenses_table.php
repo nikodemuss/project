4a94bf1a0ad9e7a7764d7daeef49526ab6086176
Niko <teddybear_happyboy@yahoo.co.id>
2019-08-27 00:18:16 +0700

    add expense model & migration

---
+++ database/migrations/2019_08_26_170620_create_expenses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('user_id');
+            $table->string('category');
+            $table->string('currency');
+            $table->string('vendor');
+            $table->string('description')->nullable();
+            $table->unsignedBigInteger('subtotal');
+            $table->unsignedBigInteger('tax')->nullable();
+            $table->unsignedBigInteger('grand_total');
+            $table->timestamps();
+
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
---
+++ resources/views/expenses/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <p>Expenses</p>
+    <div class="d-flex">
+        <p>Expense Name</p>
+        <p>Type</p>
+        <p>Vendor</p>
+        <p>Date</p>
+        <p>Subtotal</p>
+        <p>Grand Total</p>
+    </div>
+</div>
+@endsection
