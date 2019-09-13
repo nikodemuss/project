6eaebe780d6db2ad76c2e3a75f4942fa0a3f128d
Niko <teddybear_happyboy@yahoo.co.id>
2019-09-13 15:07:03 +0700

    edit company view

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -4,28 +4,33 @@
 <div class="container">
     @foreach ($companies as $company)
 
-    <div class="">
-        <div>
-            <p>{{ $company->name }}</p>
+    <a href="/company/{{ $company->id}}">
+        <div class="card mb-4">
+            <div class="card-body">
+                <h4 class="card-title">{{ $company->name }}</h4>
+                <p class="card-text">{{ $company->category }}</p>
+                <p class="card-text">{{ $company->country }}</p>
+                <a href="{{ $company->url }}" class="card-link">{{ $company->url }}</a>
+            </div>
         </div>
+    </a>
 
-        <br>
-        <p>Category {{ $company->category }}</p>
-        <br>
-        <p>Phone {{ $company->phone }}</p>
-        <br>
-        <p>Street {{ $company->street }}</p>
-        <br>
-        <p>City {{ $company->city }}</p>
-        <br>
-        <p>State {{ $company->state }}</p>
-        <br>
-        <p>Zip Code {{ $company->zipCode }}</p>
-        <br>
-        <p>Country {{ $company->country }}</p>
-        <br>
-        <p>Currency {{ $company->currency }}</p>
-    </div>
+    {{-- <br>
+    <p>Category </p>
+    <br>
+    <p>Phone {{ $company->phone }}</p>
+    <br>
+    <p>Street {{ $company->street }}</p>
+    <br>
+    <p>City {{ $company->city }}</p>
+    <br>
+    <p>State {{ $company->state }}</p>
+    <br>
+    <p>Zip Code {{ $company->zipCode }}</p>
+    <br>
+    <p>Country {{ $company->country }}</p>
+    <br>
+    <p>Currency {{ $company->currency }}</p> --}}
 
     @endforeach
 </div>
