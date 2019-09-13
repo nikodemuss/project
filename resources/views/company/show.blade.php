4b2f64d050b2371095a5ff8e36b4c5f91d4a5bbb
Niko <teddybear_happyboy@yahoo.co.id>
2019-09-13 14:37:48 +0700

    add show in company

---
+++ resources/views/company/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Show </h1>
+    <div class="">
+        <div>
+            <p>Name: {{ $company[0]->name }}</p>
+        </div>
+
+        <br>
+        <p>Category: {{ $company[0]->category }}</p>
+        <br>
+        <p>Phone: {{ $company[0]->phone }}</p>
+        <br>
+        <p>Street: {{ $company[0]->street }}</p>
+        <br>
+        <p>City: {{ $company[0]->city }}</p>
+        <br>
+        <p>State: {{ $company[0]->state }}</p>
+        <br>
+        <p>Zip Code: {{ $company[0]->zipCode }}</p>
+        <br>
+        <p>Country: {{ $company[0]->country }}</p>
+        <br>
+        <p>Currency: {{ $company[0]->currency }}</p>
+    </div>
+</div>
+@endsection
