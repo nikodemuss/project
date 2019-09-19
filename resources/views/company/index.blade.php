34af995edfc173b46cba63fad72a703000a5a24a
Niko <nikodemus.stanley@gmail.com>
2019-09-19 10:44:28 +0700

    edit app.blade layout (navbar) view

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -3,42 +3,24 @@
 @section('content')
 <div class="container">
 
-    <form method="POST" action="/search">
-        @csrf
-
-        <div class="form-group row">
-            <label for="search" class="col-md-4 col-form-label text-md-right">{{ __('Search') }}</label>
-
-            <div class="col-md-6">
-                <input id="search" type="text" class="form-control @error('search') is-invalid @enderror" name="search"
-                    value="" required autocomplete="search" autofocus>
-
-                @error('search')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="form-group row mb-0">
-            <div class="col-md-6 offset-md-4">
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Search') }}
-                </button>
-            </div>
-        </div>
-    </form>
-
+    <div class="d-flex justify-content-between">
+        <p>Company Name</p>
+        <p>Category</p>
+        <p>City</p>
+        <p>Country</p>
+    </div>
     @foreach ($companies as $company)
 
     <a href="/company/{{ $company->id}}">
         <div class="card mb-4">
             <div class="card-body">
+                <a href="/company/{{ $company->id}}" class="d-flex justify-content-between">
                 <h4 class="card-title">{{ $company->name }}</h4>
                 <p class="card-text">{{ $company->category }}</p>
+                <p class="card-text">{{ $company->city }}</p>
                 <p class="card-text">{{ $company->country }}</p>
-                <a href="{{ $company->url }}" class="card-link">{{ $company->url }}</a>
+                <!-- <a href="{{ $company->url }}" class="card-link">{{ $company->url }}</a> -->
+                </a>
             </div>
         </div>
     </a>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,13 +18,16 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm" id="navbar">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <!-- {{ config('app.name', 'Laravel') }} -->
+                    <b>Project</b>One
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -33,11 +36,42 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                    <form method="POST" action="/search">
+        @csrf
+        <div class="d-flex ml-3 align-middle">
+        <div class="form-group row mb-0">
+            <!-- <label for="search" class="col-md-4 col-form-label text-md-right">{{ __('Search') }}</label> -->
+
+            <div class="">
+                <input id="search" type="text" class="form-control @error('search') is-invalid @enderror form-border-20" name="search"
+                    value="" required autocomplete="search" autofocus>
+
+                @error('search')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row mb-0">
+            <div class="col-md-6 offset-md-4">
+                <button type="submit" class="btn btn-primary form-border-20 fa fa-search">
+                    <!-- {{ __('Search') }} -->
+                    <!-- <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg> -->
+                </button>
+            </div>
+        </div>
+</div>
+    </form>
 
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+
+                        
+                    
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
